ed8440be964c1c46ba9e3866aee93db53c5a8c59
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-13 13:03:15 +0300

    Add AppConfig

---
+++ src/main/java/org/example/app/config/AppConfig.java
@@ -0,0 +1,10 @@
+package org.example.app.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example.app")
+public class AppConfig {
+}
